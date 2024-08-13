57bcdf04945f3817ce8689e5496c1b9381c79246
Anthony Hilario <anthonyhilario.dev@gmail.com>
2024-08-13 17:51:52 -0300

    novos ajustes

---
+++ app/camera.tsx
@@ -0,0 +1,26 @@
+import { StatusBar, Text, View, Image, TextInput, StyleSheet } from "react-native";
+import { styles } from "@/styles/styles";
+import QRCodeScanner from "react-native-qrcode-scanner";
+import { Camera, useCameraPermission, useCameraDevice } from 'react-native-vision-camera';
+import { useRef } from "react";
+
+export default function Animais() {
+    const device = useCameraDevice('back');
+    const {hasPermission, requestPermission} = useCameraPermission();
+    const cameraRef = useRef<Camera>(null);
+    if(!device || device == null) return <View></View>
+
+  return (
+    <View style={styles.container} >
+        <Camera
+            style={StyleSheet.absoluteFill}
+            ref={cameraRef}
+            device={device}
+            isActive={true}
+            resizeMode="cover"
+        />
+
+        <StatusBar barStyle={'dark-content'} hidden />
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
-import { StatusBar, Text, View, Image, TouchableOpacity } from "react-native";
+import { StatusBar, Text, View, Image, TouchableOpacity, Button } from "react-native";
 import { Link } from "expo-router";
 import { styles } from "@/styles/styles";
 
 export default function Index() {
+  
+
+
   return (
   <View style={styles.container} >
     <Text style={styles.titulo} >Enciclopédia Animal</Text>
@@ -22,15 +25,17 @@ export default function Index() {
     </View>
 
     <View style={styles.row}>
-      <TouchableOpacity style={[styles.button, styles.button2]}>
-        <Text style={styles.buttonText}><Image source={require('@/assets/images/camera.png')}/></Text>
-      </TouchableOpacity>
+      <Link href={'camera'}>
+        <TouchableOpacity style={[styles.button, styles.button2]}>
+          <Text style={styles.buttonText}><Image source={require('@/assets/images/camera.png')}/></Text>
+        </TouchableOpacity>
+      </Link>
     </View>
 
     <Image style={styles.img} source={require('@/assets/images/arara-azul.jpg')} />
 
 
-    <StatusBar barStyle={'dark-content'} />
+    <StatusBar barStyle={'dark-content'} hidden />
   </View>
   );
 }
