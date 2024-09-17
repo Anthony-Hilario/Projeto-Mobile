f7fe363abec102465f744e425569c76d09831e81
Anthony Hilario <137835837+Anthony-Hilario@users.noreply.github.com>
2024-09-17 16:21:49 -0300

    Add files via upload

--- DB/src/routes.js
+++ DB/src/routes.js
@@ -14,6 +14,21 @@ routes.get('/animais', async (req, res) => {
   }
 });
 
+app.get('/:continente', async (req, res) => {
+  const { continente } = req.params; // Pega o continente a partir dos parâmetros da URL
+  try {
+    // Busca os animais pelo continente, retornando apenas os campos 'nome_animal', 'imagem' e 'continente'
+    const animais = await animals.find(
+      { continente }, // Filtro: animais que pertencem ao continente
+      'nome imagem continente' // Projeção: apenas esses campos serão retornados
+    );
+    res.status(200).json(animais); // Retorna os animais filtrados
+  } catch (error) {
+    res.status(500).json({ error: error.message }); // Envia uma resposta de erro se houver falha
+  }
+});
+
+
 
 
 
